5a8d435500aa494ea4b0dc6d0b76c4e0b46e6b90
iantoon0 <ihunickesmith@gmail.com>
2016-12-29 18:32:14 -0600

    Added first screen

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public RectTransform pitScoutTransform, matchScoutTransform, analyticsTransform;
+    public Text titleText;
+	// Use this for initialization
+	void Start () {
+        Text[] textArray = GetComponentsInChildren<Text>();
+        titleText = textArray[3];
+        RectTransform[] rectTransformArray = GetComponentsInChildren<RectTransform>();
+        pitScoutTransform = rectTransformArray[1];
+        matchScoutTransform = rectTransformArray[3];
+        analyticsTransform = rectTransformArray[5];
+        Vector2 rectSize = titleText.GetComponent<RectTransform>().sizeDelta;
+        rectSize = new Vector2(rectSize.x, (1/4) * Screen.height);
+        titleText.rectTransform.sizeDelta = rectSize;
+        pitScoutTransform.sizeDelta = rectSize;
+        matchScoutTransform.sizeDelta = rectSize;
+        analyticsTransform.sizeDelta = rectSize;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+}
