c8e830aa391e69ba4c12e1bc91d2f6143bf69a01
iantoon0 <ihunickesmith@gmail.com>
2017-01-03 16:50:06 -0600

    Updated to Unity 5.5

---
+++ Assets/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject matchScoutPanel, pitScoutPanel, analyticsPanel, openPanel;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+    void openPitScoutPanel()
+    {
+
+    }
+    void openMatchScoutPanel()
+    {
+
+    }
+    void openAnalyticsPanel()
+    {
+
+    }
+}
