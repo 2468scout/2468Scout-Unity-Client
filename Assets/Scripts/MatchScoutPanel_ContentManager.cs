d132ade4c01583fff8a586ff3c7531931d7b0e9a
iantoon0 <ihunickesmith@gmail.com>
2017-01-05 08:39:09 -0600

    Began adding infrastructure for server communication

--- Assets/Scripts/MatchScoutPanel_ContentManager.cs
+++ Assets/Scripts/MatchScoutPanel_ContentManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class MatchScoutPanel_ContentManager : MonoBehaviour {
 
+    TeamMatch currentlyScoutingTeamMatch;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,11 @@ public class MatchScoutPanel_ContentManager : MonoBehaviour {
 	void Update () {
 		
 	}
+    public void saveTeamMatch()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + currentlyScoutingTeamMatch.sFileName + ".dat");
+        bf.Serialize(file, null);
+        file.Close();
+    }
 }
---
+++ Assets/Scripts/TeamMatch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[System.Serializable]
+public class TeamMatch{
+    public int iTeamNumber, iMatchNumber, iAllianceNumber;
+    public string sNotes, sFileName, sEventCode;
+    public bool bColor; // False is blue, true is red
+    // GAME SPECIFIC ELEMENTS
+
+}
