32b5bee39afc29a7ed1cc9746ad96c6ab8232e7e
mightypine <morganelizabethbonner@gmail.com>
2017-01-10 15:59:59 -0600

    Began adding teamdatapanel

---
+++ Assets/Scripts/PanelScripts/TeamDataPanelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class TeamDataPanelManager : MonoBehaviour
+    {
+        Team team;
+        Text teamNameNumberText;
+        // Use this for initialization
+        void Start()
+        {
+            Text[] textArray = GetComponentsInChildren<Text>();
+            teamNameNumberText = textArray[0];
+            teamNameNumberText.text = "lol nerd";
+            /*
+             * team name
+             * team number
+             * picture
+             * statistics?
+             * 
+             */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
