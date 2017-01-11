f6b3fdddd81caed14ae51dc598ca34ae06acff7c
mightypine <morganelizabethbonner@gmail.com>
2017-01-11 12:08:47 -0600

    Added teamdatapanel to uimanager

--- Assets/Scripts/PanelScripts/TeamDataPanelManager.cs
+++ Assets/Scripts/PanelScripts/TeamDataPanelManager.cs
@@ -7,21 +7,21 @@ namespace Assets.Scripts
 {
     public class TeamDataPanelManager : MonoBehaviour
     {
+        UIManager manager;
         Team team;
-        Text teamNameNumberText;
+        Text teamNameNumberText, backButtonText;
+        Button backButton;
         // Use this for initialization
         void Start()
         {
+            manager = GetComponentInParent<UIManager>();
             Text[] textArray = GetComponentsInChildren<Text>();
             teamNameNumberText = textArray[0];
             teamNameNumberText.text = "lol nerd";
-            /*
-             * team name
-             * team number
-             * picture
-             * statistics?
-             * 
-             */
+            backButtonText = textArray[1];
+            backButton = GetComponentInChildren<Button>();
+            backButtonText.text = "Back";
+            backButton.onClick.AddListener(() => { manager.ChangePanel("analyticsPanel"); });
         }
 
         // Update is called once per frame
