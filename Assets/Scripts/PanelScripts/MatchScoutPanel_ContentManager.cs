574135f2bd8aacd7c056a33afb988e0a5c934227
iantoon0 <ihunickesmith@gmail.com>
2017-01-12 21:09:01 -0600

    Added PointEventButtonPanel, continued work on MatchScoutPanel

--- Assets/Scripts/PanelScripts/MatchScoutPanel_ContentManager.cs
+++ Assets/Scripts/PanelScripts/MatchScoutPanel_ContentManager.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using UnityEngine.UI;
+
 namespace Assets.Scripts{
     public class MatchScoutPanel_ContentManager : MonoBehaviour
     {
         string teamMatchURL = "http://frc2468.org/matchUpload";
-        GameObject fieldPanel;
-        RectTransform fieldPanelRectTransform;
+        GameObject fieldImage;
+        RectTransform fieldImageRectTransform;
         float aspectRatio;
         TeamMatch currentlyScoutingTeamMatch;
+        Button backButton, matchStartButton, menuButton, fieldImageButton;
         // Use this for initialization
         void Start()
         {
-            fieldPanel = GameObject.Find("FieldPanel");
-            fieldPanelRectTransform = fieldPanel.GetComponent<RectTransform>();
-            aspectRatio = (fieldPanelRectTransform.rect.height) / fieldPanelRectTransform.rect.width;
+            fieldImage = GameObject.Find("FieldImage");
+            fieldImageRectTransform = fieldImage.GetComponent<RectTransform>();
+            fieldImageRectTransform.offsetMin = new Vector2(0, -(GameObject.Find("FieldPanel").GetComponent<RectTransform>().rect.width /2048) * 784 / 2);
+            fieldImageRectTransform.offsetMax = new Vector2(0, (GameObject.Find("FieldPanel").GetComponent<RectTransform>().rect.width / 2048) * 784 / 2);
+            Debug.Log("FieldPanel width:" + GameObject.Find("FieldPanel").GetComponent<RectTransform>().rect.width + ", FieldPanel height: " + GameObject.Find("FieldPanel").GetComponent<RectTransform>().rect.height + ", calculated height:"  + ((GameObject.Find("FieldPanel").GetComponent<RectTransform>().rect.width / 2048) * 784));
+            Button[] buttonArray = GetComponentsInChildren<Button>();
+            backButton = buttonArray[0];
+            menuButton = buttonArray[1];
+            matchStartButton = buttonArray[2];
+            fieldImageButton = buttonArray[3];
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(aspectRatio != 784 / 2048)
-            {
-                if (aspectRatio > 784 / 2048) //Taller than it should be
-                {
-                    int rightHeight = (int)((fieldPanelRectTransform.rect.width / 2048) * (784));
-                    int heightMod = (int)(rightHeight - fieldPanelRectTransform.rect.height);
-                    fieldPanelRectTransform.offsetMax = new Vector2(0, heightMod / 2);
-                    fieldPanelRectTransform.offsetMin = new Vector2(0, - (heightMod / 2));
-                    aspectRatio = (fieldPanelRectTransform.rect.height) / fieldPanelRectTransform.rect.width;
-                }
-                else                         //Wider than it should be
-                {
-                    int rightWidth = (int)((fieldPanelRectTransform.rect.height / 784) * (2048));
-                    int widthMod = (int)(rightWidth - fieldPanelRectTransform.rect.width);
-                    fieldPanelRectTransform.offsetMax = new Vector2(widthMod/2, 0);
-                    fieldPanelRectTransform.offsetMin = new Vector2(- (widthMod/2), 0);
-                    aspectRatio = (fieldPanelRectTransform.rect.height) / fieldPanelRectTransform.rect.width;
-                }
-            }
+            
         }
         public void SaveTeamMatch()
         {
@@ -68,6 +60,11 @@ namespace Assets.Scripts{
             {
                 print("Success!");
             }            
+        }
+
+        public void CreatePointMatch()
+        {
+
         }
     }
 }
